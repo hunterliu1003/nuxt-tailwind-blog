5e5ae4afdf977ceb662d880d6036101651358b3b
Hunter <hunterliu1003@gmail.com>
2020-04-06 20:36:00 +0800

    init

---
+++ nuxt.config.js
@@ -0,0 +1,57 @@
+const shrinkRay = require('shrink-ray-current')
+
+module.exports = {
+  mode: 'universal',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  loading: { color: '#fff' },
+  css: [],
+  plugins: [],
+  buildModules: [
+    '@nuxtjs/dotenv',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.(css|vue)$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  // brotli 壓縮
+  render: {
+    compressor: shrinkRay()
+  },
+  // 啟用 http2
+  http2: {
+    push: true
+  },
+  // nuxt-compress is a simple asset compression module for Gzip and Brotili
+  modules: ['nuxt-compress'],
+  'nuxt-compress': {
+    gzip: {
+      cache: true
+    },
+    brotli: {
+      threshold: 10240
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template lang="pug">
+  div
+    h1.mt-4 nuxt-tailwind-blog
+    h2.mt-4 My splendiferous Nuxt.js project
+</template>
