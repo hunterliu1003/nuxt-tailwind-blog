77a097d91b1940ae8faffc4dfa356f64dc9376f9
Hunter <hunterliu1003@gmail.com>
2020-04-24 19:18:10 +0800

    add header-link

--- components/HHeader.vue
+++ components/HHeader.vue
@@ -1,4 +1,14 @@
 <template lang="pug">
   nav.flex
-    nuxt-link(to='/').font-semibold.text-xl.tracking-tight.px-6.py-2 Hunter Liu
+    nuxt-link(to='/').header-link Hunter Liu
+    .flex-grow
+    nuxt-link(to='/').header-link about me
+    nuxt-link(to='/').header-link articles
+    nuxt-link(to='/').header-link tags
 </template>
+
+<style scoped>
+.header-link {
+  @apply font-semibold text-xl tracking-tight px-6 py-2;
+}
+</style>
