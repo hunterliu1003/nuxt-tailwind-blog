ff1ae0417fe1dc47d6717771e15a2fad6b70e2f9
Hunter <hunterliu1003@gmail.com>
2020-05-06 19:25:10 +0800

    add default layout transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div
     HHeader
-    nuxt.max-w-3xl.px-4.md_px-0.mx-auto
+    nuxt.max-w-3xl.px-4.py-16.md_px-0.mx-auto
 </template>
 
 <script>
@@ -9,3 +9,16 @@ export default {
   name: 'LayoutDefault'
 }
 </script>
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition-property: opacity;
+  transition-timing-function: ease-in-out;
+  transition-duration: 0.1s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+</style>
