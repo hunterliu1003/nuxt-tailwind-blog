57b540350603275d0c9b0ca09081428ad7c69d76
Hunter <hunterliu1003@gmail.com>
2020-05-06 19:34:59 +0800

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  main.text-center
+    h1 404
+    p.my-4 此頁面不存在
+    p 如果你是因為 bug 而進到這頁面
+    p 可以透過 
+      a(href="https://github.com/hunterliu1003/nuxt-tailwind-blog") Github issue
+      |  回報錯誤
+    p 我請你吃飯，交個朋友
+</template>
