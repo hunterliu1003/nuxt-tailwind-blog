8b908af21af54afb687914021fcfe919c756210e
Hunter <hunterliu1003@gmail.com>
2020-05-07 20:01:41 +0800

    fetch encodeURI

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -1,7 +1,7 @@
 export default ({ error }, inject) => {
   inject('fetch', (url, data) => {
     const baseUrl = process.server ? process.env.BASE_URL : window.location.origin
-    return fetch(baseUrl + url, Object.assign({ method: 'GET' }, data))
+    return fetch(encodeURI(baseUrl + url), Object.assign({ method: 'GET' }, data))
       .then(res => res.json())
       .catch(e => {
         error({ statusCode: 404, message: 'Post not found' })
