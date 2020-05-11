26d6be175f4e6591fc53024f7c5e8381db69bcf2
Hunter <hunterliu1003@gmail.com>
2020-05-11 14:00:29 +0800

    update footer

--- components/HFooter.vue
+++ components/HFooter.vue
@@ -3,6 +3,7 @@
     p.text-sm.font-normal.text-center
       | Copyright © {{ new Date().getFullYear() }} 
       a(href="/") HunterLiu's Blog. 
+    p.text-sm.font-normal.text-center
       | Icons made by 
       a(href='https://www.flaticon.com/authors/freepik', title='Freepik') Freepik, 
       a(href='https://www.flaticon.com/authors/pixel-perfect', title='Pixel perfect') Pixel perfect
