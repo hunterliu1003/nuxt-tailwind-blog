df039e550db915deb2ce0d50da4011eab4024244
Hunter <hunterliu1003@gmail.com>
2020-05-14 19:49:16 +0800

    dark mode HTag

--- components/HTag.vue
+++ components/HTag.vue
@@ -1,3 +1,3 @@
 <template lang="pug" functional>
-  nuxt-link.px-2.py-1.border-solid.border.border-black.rounded(:to="`/tags/${props.tag}`" :class="data.staticClass") {{ props.tag }}
+  nuxt-link.px-2.py-1.border-solid.border.border-black.dark_border-white.rounded(:to="`/tags/${props.tag}`" :class="data.staticClass") {{ props.tag }}
 </template>
