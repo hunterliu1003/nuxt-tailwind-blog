473f0fa3a7fe3560cb68c0a5ac61d71cb25939d1
Hunter <hunterliu1003@gmail.com>
2020-05-18 01:42:07 +0800

    add tailwind color nuxt

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,11 @@ module.exports = {
     darkSelector: '.dark-mode',
     extend: {
       colors: {
+        nuxt: {
+          gray: '#2F495E',
+          lightgreen: '#00C58E',
+          green: '#108775'
+        },
         light: {
           surface: '#F8FAFC',
           onSurfacePrimary: '#2F495E',
