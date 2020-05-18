4ee60cf4e02128ea356e5f396e69cf56d1de30d6
Hunter <hunterliu1003@gmail.com>
2020-05-18 18:05:18 +0800

    add rwd HTags

--- components/HTags.vue
+++ components/HTags.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div
-    .flex.flex-wrap.-mb-4
-      HTag.block.mb-4.mr-4.last_mr-0(v-for="tag in tags" :key="tag" :tag="tag")
+    .flex.flex-wrap.-mb-2.md_-mb-4
+      HTag.block.mb-2.md_mb-4.mr-2.md_mr-4.last_mr-0(v-for="tag in tags" :key="tag" :tag="tag")
 </template>
 
 <script>
