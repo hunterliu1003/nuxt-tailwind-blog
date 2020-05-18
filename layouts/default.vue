1d987c69e5d59fa6387154dc9c0b8484c60287ab
Hunter <hunterliu1003@gmail.com>
2020-05-18 20:00:32 +0800

    fixed style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div.flex.flex-col.min-h-screen.bg-light-elevatedSurface.dark_bg-dark-elevatedSurface
     HHeader
-    main.w-full.max-w-3xl.px-8.pb-16.pt-16.md_pt-24.mx-auto
+    main.w-full.max-w-3xl.px-4.pb-16.pt-16.md_pt-24.mx-auto
       nuxt.pt-8
     HFooter.mt-auto.mb-16.md_mb-0
     HMobileMainNavigation
