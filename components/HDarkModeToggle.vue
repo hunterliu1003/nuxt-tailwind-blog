b243390e2ff81278e7547c0c66dbf716c1bd9179
Hunter <hunterliu1003@gmail.com>
2020-05-19 14:18:28 +0800

    update HDarkModeToggle

--- components/HDarkModeToggle.vue
+++ components/HDarkModeToggle.vue
@@ -1,11 +1,8 @@
 <template lang="pug">
-  button.relative.flex.items-center.h-10.px-4.bg-gray-200.dark_bg-dark-surface.dark_text-dark-onSurfaceSecondary.rounded-full.text-light-onSurfacePrimary.dark_text-dark-onSurfacePrimary.transition-colors.duration-300.ease-linear(@click='setCurrentTheme')
-    span.relative.w-5.h-5.mr-2
+  button.relative.flex.items-center.h-10.px-4.bg-gray-200.dark_bg-dark-surface.dark_text-dark-onSurfaceSecondary.rounded-full.text-light-onSurfacePrimary.dark_text-dark-onSurfacePrimary.focus_outline-none.transition-colors.duration-300.ease-linear(@click.prevent='setCurrentTheme')
+    span.relative.w-5.h-5
       SvgMoon(class="w-5 h-5 absolute" :class="$colorMode.preference === 'dark' ? 'show' : 'hide'")
       SvgSun(class="w-5 h-5 absolute" :class="$colorMode.preference === 'light' ? 'show' : 'hide'")
-    transition(name="from-bottom-to-bottom" mode="out-in")
-      span.mr-1(v-if="$colorMode.preference === 'dark'" key="dark") Dark
-      span.mr-1(v-else-if="$colorMode.preference === 'light'" key="light") Light
 </template>
 
 <script>
@@ -19,10 +16,6 @@ export default {
 </script>
 
 <style scoped>
-button {
-  outline: none;
-}
-
 .show {
   animation: show-icon 300ms forwards;
 }
@@ -51,25 +44,5 @@ button {
     opacity: 0;
     transform: scaleY(0);
   }
-}
-
-.from-bottom-to-bottom-enter-active {
-  opacity: 1;
-  transition: opacity 100ms, transform 100ms cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-.from-bottom-to-bottom-leave-active {
-  opacity: 1;
-  transition: opacity 100ms, transform 100ms cubic-bezier(0.4, 0, 0.2, 1);
-}
-
-.from-bottom-to-bottom-enter {
-  opacity: 0;
-  transform: scaleY(0);
-}
-
-.from-bottom-to-bottom-leave-active {
-  opacity: 0;
-  transform: scaleY(0);
 }
 </style>
