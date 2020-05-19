7f58c41731cb526392eec83a548f3fba8776ba62
Hunter <hunterliu1003@gmail.com>
2020-05-19 17:25:14 +0800

    remove VueDisqus and add HLazyDisqus, HDisqusCommentCounter

--- components/pages/post.vue
+++ components/pages/post.vue
@@ -1,53 +1,31 @@
 <template lang="pug">
-  section
+  article
     h1 {{ post.data.title }}
     p {{ $filter.getMMMDDYYYY(post.timestamp) }}
     HTags.my-4(:tags="post.data.tags")
-    ul.flex
+    ul.flex.pb-5.border-b.border-light-border.dark_border-dark-border
       li
-        a(:href="`${href}#disqus_thread`") 0
-        span  留言
+        HViewCounter
       li.ml-4 fb
       li.ml-4 vue-goodshare
-      li.ml-4
-        HViewCounter
-    hr
+      li.ml-auto
+        HDisqusCommentCounter(shortname="hunterliu-blog")
     HMarkdown.mb-4(v-once :value="post.content")
     .flex.justify-between.my-12.pb-5.border-b.border-light-border.dark_border-dark-border
       NLink.font-sans.text-primary-base(v-if="post.prevPost" to="/" :to="post.prevPost.routePath || ''") ← {{ post.prevPost.data.title }}
       .flex-grow
       NLink.font-sans.text-primary-base(v-if="post.nextPost" to="/" :to="post.nextPost.routePath || ''") {{ post.nextPost.data.title }} →
-    client-only
-      VueDisqus(shortname="hunterliu-blog" :identifier="$route.fullPath")
+    HLazyDisqus(shortname="hunterliu-blog" :identifier="$route.fullPath")
     h2.mt-12.mb-4 近期發文
     h3
       NLink.block(:to="recentPost.routePath" v-for="recentPost in recentPostsRoutes" :key="recentPost.routePath")  {{ $filter.getMMMDDYYYY(recentPost.timestamp) }} - {{ recentPost.data.title }}
 </template>
 
 <script>
-import VueDisqus from 'vue-disqus/src/vue-disqus.vue'
-
 export default {
   name: 'PagePost',
-  components: {
-    VueDisqus
-  },
   asyncData(context) {
     return Object.freeze(context.app.$fetch(context, `/api/post?path=posts/${context.route.params.pathMatch}`))
-  },
-  data: () => ({
-    href: ''
-  }),
-  watch: {
-    '$colorMode.preference'() {
-      setTimeout(() => {
-        window.DISQUS && window.DISQUS.reset({ reload: true })
-      }, 300)
-    }
-  },
-  mounted() {
-    this.href = window.location.href
-    window.DISQUSWIDGETS && window.DISQUSWIDGETS.getCount({ reset: true })
   },
   head() {
     return {
@@ -59,8 +37,7 @@ export default {
         { hid: 'og:description', property: 'og:description', content: this.post.data.description },
         { hid: 'og:type', property: 'og:type', content: 'article' },
         ...this.post.data.meta
-      ],
-      script: [{ src: 'https://hunterliu-blog.disqus.com/count.js', id: 'dsq-count-scr', async: true, body: true }]
+      ]
     }
   }
 }
