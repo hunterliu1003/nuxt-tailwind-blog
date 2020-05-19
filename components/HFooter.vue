ae8277ccddbd6b53b4f1223dba509bb5ca26ea0a
Hunter <hunterliu1003@gmail.com>
2020-05-19 17:19:54 +0800

    udpate footer style

--- components/HFooter.vue
+++ components/HFooter.vue
@@ -2,11 +2,11 @@
   footer.p-4
     p.text-sm.font-normal.text-center
       | Copyright © {{ new Date().getFullYear() }} 
-      a(href="/") HunterLiu's Blog. 
+      a.text-primary-base(href="/") HunterLiu's Blog. 
     p.text-sm.font-normal.text-center
       | Icons made by 
-      a(href='https://www.flaticon.com/authors/freepik', title='Freepik') Freepik, 
-      a(href='https://www.flaticon.com/authors/pixel-perfect', title='Pixel perfect') Pixel perfect 
+      a.text-primary-base(href='https://www.flaticon.com/authors/freepik', title='Freepik') Freepik, 
+      a.text-primary-base(href='https://www.flaticon.com/authors/pixel-perfect', title='Pixel perfect') Pixel perfect 
       | from 
-      a(href='https://www.flaticon.com/', title='Flaticon') flaticon
+      a.text-primary-base(href='https://www.flaticon.com/', title='Flaticon') flaticon
 </template>
