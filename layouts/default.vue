3449690ae4c655a54d05915084e3b9ae7b9077d3
Hunter <hunterliu1003@gmail.com>
2020-05-20 14:04:18 +0800

    update HScrollTop style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   div.flex.flex-col.min-h-screen.bg-light-elevatedSurface.dark_bg-dark-elevatedSurface
     HHeader
-    main.w-full.max-w-3xl.px-4.pb-16.pt-16.md_pt-24.mx-auto
+    main.w-full.max-w-3xl.mx-auto.px-4.pb-16.pt-16.md_pt-24
       nuxt.pt-8
     HFooter.mt-auto.mb-16.md_mb-0
     HMobileMainNavigation
-    HScrollTop
+    HScrollTop.w-full.max-w-3xl.mx-auto.mb-16.md_mb-0
 </template>
 
 <script>
