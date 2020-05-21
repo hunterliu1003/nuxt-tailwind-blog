8ec1a9c69b652dfc9555daa2dbb77b3ff4a3fb73
Hunter <hunterliu1003@gmail.com>
2020-05-21 20:02:06 +0800

    fixed line-height

--- components/HPostList.vue
+++ components/HPostList.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   div
     NLink.flex.items-center.py-4.leading-none(v-for="post in postList" :key="post.routePath" :to="post.routePath")
-      component(:is="headingTag").mr-auto.font-normal {{ post.data.title }}
+      component(:is="headingTag").mr-auto.font-normal.leading-normal {{ post.data.title }}
       span.pl-2.text-right.text-xs.whitespace-no-wrap {{ $filter.getMMMDDYYYY(post.timestamp) }}
 </template>
 
