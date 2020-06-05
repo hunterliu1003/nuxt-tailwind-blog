b66f5a54c5b4879c7293c80ce56318b41fd9a9d4
Hunter <hunterliu1003@gmail.com>
2020-06-05 19:24:27 +0800

    add IconGithub

--- components/HHeader.vue
+++ components/HHeader.vue
@@ -3,15 +3,16 @@
     .max-w-3xl.flex.items-center.h-full.mx-auto.px-4
       h1.mr-auto
         NLink(to="/posts").block.text-2xl.tracking-tight HunterLiu
-      nav.hidden.md_flex
+      nav.hidden.md_flex.md_items-center
         h2.m-0.h-0.w-0.overflow-hidden 關於我、文章列表和標籤列表
         NLink(to="/").header-link 關於我
         NLink(to="/posts").header-link 文章列表
         NLink(to="/tags").header-link 標籤列表
+      a.ml-2.hover_text-primary-dark(href='https://github.com/hunterliu1003' target='_blank' rel='noopener noreferrer' title='Github' name='Github')
+        IconGithub.w-6.h-6
+      HDarkModeToggle.ml-2
       //- .flex.justify-center
         a.header-link(rel="noopener noreferrer" href="https://www.facebook.com/hunterliu1003" target="_blank") Facebook
-        a.header-link(rel="noopener noreferrer" href="https://github.com/hunterliu1003" target="_blank") Github
-      HDarkModeToggle.ml-2
 </template>
 
 <script>
@@ -22,6 +23,6 @@ export default {
 
 <style scoped>
 .header-link {
-  @apply block text-xl tracking-tight px-6 py-2;
+  @apply block px-4 py-2 text-xl tracking-tight underline;
 }
 </style>
